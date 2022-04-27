744a8c4649bdeb4d13108ca18f67c04c8e231723
Robert Silve <robert@silve.net>
2022-04-27 11:40:23 +0000

    fix conf

--- react-clock/vite.config.ts
+++ react-clock/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 let server = 'ws://localhost:8080';
-let hmr = {};
 if (process.env.GITPOD_GIT_USER_EMAIL) {
-  hmr = { port: 443 };
   server = server = `ws://${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
 }
 
@@ -12,9 +10,7 @@ if (process.env.GITPOD_GIT_USER_EMAIL) {
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: hmr,
-  },
+  
   define: {
     __APP_BACKEND__: JSON.stringify(server),
   },
